3234c819edd43629011e68c43611c9e158f0d52e
Ramachandra <you@example.com>
2025-01-07 17:49:55 +0530

    files added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(name="census_income",
+      version="0.0.1",
+      author="Ramachandran_D",
+      author_email="sureshchandran2209@gmail.com",
+      packages=find_packages(),
+      install_requires = ["numpy","pandas","seaborn"]
+      )
+    
